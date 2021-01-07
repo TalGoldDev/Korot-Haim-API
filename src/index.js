c3642931abe122c4a2828409dfe82a355b3d20ee
Tal Gold <talpogorelis@gmail.com>
2021-01-07 13:14:05 +0200

    modified usage of accescontrol middleware

--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@ import express from "express";
 import { accessControlAllowOrigin } from "../middleware";
 
 const app = express();
+app.use(accessControlAllowOrigin);
 
-app.post("/", accessControlAllowOrigin, function (req, res) {
+app.post("/", function (req, res) {
   res.send("hello-world");
 });
 

e6d0b721003bd4017726e811d7088498f33512ae
Tal Gold <talpogorelis@gmail.com>
2021-01-11 23:21:25 +0200

    Basic pdf generation from html

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
 import app from "./src";
 import puppeteer from "puppeteer";
-import fs from "fx-extra";
+import fs from "fs-extra";
+
+(async function () {
+  try {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+
+    await page.setContent("<h1>שלום</h1>");
+    await page.emulateMediaType("screen");
+    await page.pdf({
+      path: "mypdf.pdf",
+      format: "A4",
+      printBackground: true,
+    });
+
+    console.log("done");
+    await browser.close();
+    process.exit();
+  } catch (e) {
+    console.log("error", e);
+  }
+})();
 // const port = process.env.PORT || 3001;
 
 // const server = app.listen(port);

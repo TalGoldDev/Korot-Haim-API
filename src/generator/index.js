00e36d7499c1c69457f973199d8491590b274e4f
Tal Gold <talpogorelis@gmail.com>
2021-01-21 04:07:34 +0200

    API now generates PDF using data sent from client

--- src/generator/index.js
+++ src/generator/index.js
@@ -3,20 +3,13 @@ import fs from "fs-extra";
 import hbs from "handlebars";
 import path from "path";
 
-const compile = async function (templateName, data) {
-  const filePath = path.join(__dirname, "templates", `${templateName}.hbs`);
-  const html = await fs.readFile(filePath, "utf-8");
-  return hbs.compile(html)(data);
-};
-
 const generatePDFFromHTML = async function (data) {
   try {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
 
-    const content = await compile(`template${data.selectedTemplate}`, data);
-
-    await page.setContent(content);
+    console.log(data);
+    await page.setContent(data.html);
     await page.emulateMediaType("screen");
     await page.pdf({
       path: "mypdf.pdf",
--- src/index.js
+++ src/index.js
@@ -5,11 +5,10 @@ const app = express();
 app.use(accessControlAllowOrigin);
 app.use(express.json());
 
-// app.post("/", generatePDF);
+app.post("/", generatePDF);
 
 app.post("/", (req, res) => {
-    console.log(req.body);
-    res.send();
+  res.send();
 });
 
 export default app;
